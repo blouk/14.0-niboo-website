37a1d93758fc008edd5e473b4b27a7a45a486869
Blouk <nicolas@blouk.com>
2021-02-16 09:19:33 +0100

    [WIP]

--- theme_niboo/static/src/js/main.js
+++ theme_niboo/static/src/js/main.js
@@ -1,24 +1,30 @@
-odoo.define('theme_niboo.simpleJsAction', function(require) {
-  'use strict';
+odoo.define('theme_niboo.simpleJsAction', async function(require) {
+      'use strict';
 
-  const publicWidget = require('web.public.widget');
+      const publicWidget = require('web.public.widget');
+      const core = require('web.core');
+      const qweb = core.qweb;
+      const ajax = require('web.ajax');
 
+      await ajax.loadXML('/theme_niboo/static/src/xml/modal.xml', qweb);
 
-  publicWidget.registry.simpleJsAction = publicWidget.Widget.extend({
-    selector: '.niboo-prefooter',
-    events: {
-      'click h3': '_onclickHeader'
-    },
+      publicWidget.registry.simpleJsAction = publicWidget.Widget.extend({
+          selector: '.niboo-prefooter',
+          events: {
+            'click h3': '_onclickHeader'
+          },
 
-    init: function(parent, params) {
-      this._super.apply(this, arguments);
-      this.time = 0;
-    },
+          init: function(parent, params) {
+            this._super.apply(this, arguments);
+            this.time = 0;
+          },
 
-    _onclickHeader: function(e) {
-      this.time++;
-      alert('You just clicked ' + this.time + " time(s) the header");
-    }
-
-  });
-});
+          _onclickHeader: function(e) {
+            this.time++;
+            $('.niboo-modal').remove();
+            $('body').append( $(qweb.render('niboo_theme.modal', {
+                clicks: this.time
+              })).modal());
+          }
+      });
+})
